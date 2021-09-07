1418941fe9d4326783eceaa3f6ca10ea70c721b9
Jederson Sousa Luz <jedersonalpha@gmail.com>
2021-09-07 16:43:05 -0300

    Audio Recorder

--- App.js
+++ App.js
@@ -1,12 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { Audio } from 'expo-av';
+
+import * as MediaLibrary from 'expo-media-library';
 
 export default function App() {
+  const [recording, setRecording] = React.useState();
+
+  async function startRecording() {
+    try {
+      console.log('Requesting permissions..');
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+      console.log('Starting recording..');
+      const { recording } = await Audio.Recording.createAsync(
+        Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+      );
+      setRecording(recording);
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log('Stopping recording..');
+    setRecording(undefined);
+    await MediaLibrary.requestPermissionsAsync();
+    await recording.stopAndUnloadAsync();
+    const uri = recording.getURI();
+    const asset = await MediaLibrary.createAssetAsync(uri);
+    // MediaLibrary.createAlbumAsync('Expo', asset)
+    //   .then(() => {
+    //     console.log('Album created!');
+    //   })
+    //   .catch(error => {
+    //     console.log('err', error);
+    //   });
+    console.log('Recording stopped and stored at', uri);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title={recording ? 'Stop Recording' : 'Start Recording'}
+        onPress={recording ? stopRecording : startRecording}
+      />
     </View>
   );
 }
@@ -14,8 +56,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 10,
   },
 });
